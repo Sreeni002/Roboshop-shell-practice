45bf41eee1eeef52eb36ea2318a1ab63629da0d6
Sreeni.B <sreenu.reddy002@gmail.com>
2023-04-28 07:00:30 +0530

    Making config corrections in mongo.repo

--- R_Mongodb.sh
+++ R_Mongodb.sh
@@ -1,5 +1,5 @@
 yum install mongodb-org -y
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp R_mongo.repo /etc/yum.repos.d/R_mongo.repo
 systemctl enable mongod
 systemctl start mongod
 #update mongo db listen address
