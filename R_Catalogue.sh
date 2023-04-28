23823213128b22f3bc5f1fc562262f8307212764
Sreeni.B <sreenu.reddy002@gmail.com>
2023-04-28 12:08:54 +0530

    Apply comments or printing comments

--- R_Catalogue.sh
+++ R_Catalogue.sh
@@ -1,15 +1,26 @@
+echo -e "\e[36m>>>>>>>>>>>>>>>>>Setup NodeJs<<<<<<<<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[36m>>>>>>>>>>>>>>>>>Install NodeJs<<<<<<<<<<<<<<<<<<\e[0m"
 yum install nodejs -y
+echo -e "\e[36m>>>>>>>>>>>>>>>>>Create user roboshop<<<<<<<<<<<<<<<<<<\e[0m"
 useradd roboshop
 mkdir /app
+echo -e "\e[36m>>>>>>>>>>>>>>>>>Download app content<<<<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+echo -e "\e[36m>>>>>>>>>>>>>>>>>Extract app content<<<<<<<<<<<<<<<<<<\e[0m"
 unzip /tmp/catalogue.zip
-cd /app
+echo -e "\e[36m>>>>>>>>>>>>>>>>>Install NodeJs Dependencies<<<<<<<<<<<<<<<<<<\e[0m"
 npm install
+echo -e "\e[36m>>>>>>>>>>>>>>>>>Create systemd file<<<<<<<<<<<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[36m>>>>>>>>>>>>>>>>>Load system service<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
+echo -e "\e[36m>>>>>>>>>>>>>>>>>Enable catalogue service<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable catalogue
-systemctl start catalogue
+echo -e "\e[36m>>>>>>>>>>>>>>>>>Restart catalogue service<<<<<<<<<<<<<<<<<<\e[0m"
+systemctl restart catalogue
+echo -e "\e[36m>>>>>>>>>>>>>>>>>Install Mongodb<<<<<<<<<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+echo -e "\e[36m>>>>>>>>>>>>>>>>>Load schema<<<<<<<<<<<<<<<<<<\e[0m"
+mongo --host mongodb-dev.sreenivasulareddydevops.online </app/schema/catalogue.js
